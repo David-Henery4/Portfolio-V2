12f6ba582dc6b5480d29f4f7be235a640e5a8b02
DizzlyD <david4henery00@hotmail.com>
2023-01-30 15:45:51 +0000

    added-basic-new-projects-data-(without-mockups-&-thumbs)

--- src/projects-data/projectsData.js
+++ src/projects-data/projectsData.js
@@ -72,7 +72,8 @@ const projectsData = [
     name: "Interactive Comments Section",
     descShort:
       "This is a fully interactive comments section, where users can create, edit, like, reply and delete comments",
-    descLong: "Interactive Comments Section is fully interactive comments section. where users can add, edit and delete their own messages. Users can also reply and like other messages. Messages also display a dynamic timer to keep track of when a message was posted.",
+    descLong:
+      "Interactive Comments Section is fully interactive comments section. where users can add, edit and delete their own messages. Users can also reply and like other messages. Messages also display a dynamic timer to keep track of when a message was posted.",
     thumbNail: "interactive-comments-section-thumb.png",
     deviceMockupsSml: "device-mockups-small.png",
     deviceMockupsLge: "device-mockups-large.png",
@@ -129,6 +130,112 @@ const projectsData = [
     ],
     repoUrl: "https://github.com/David-Henery4/Where-in-the-world",
     liveUrl: "https://where-abouts-in-the-world.netlify.app",
+  },
+  {
+    id: 7,
+    name: "Bookmarks",
+    descShort:
+      "A landing page for a company that sell software that helps users bookmark and orginise websites.",
+    descLong:
+      "Bookmarks is a landing page for a company that sells sfotware that helps to orginse & bookmark your website. It has multiple sections, including a tabbed features section that allows you to switch between the tabs for different features. It also has an accordion section for FAQs and also some light email validation on a contact us section.",
+    thumbNail: "where-in-the-world-thumb.png",
+    deviceMockupsSml: "device-mockups-small.png",
+    deviceMockupsLge: "device-mockups-large.png",
+    technologiesUsed: ["HTML", "CSS/SCSS", " Javascript", "TailwindCSS"],
+    repoUrl: "https://github.com/David-Henery4/Bookmark-Landing",
+    liveUrl: "https://bookmarks-home-landing.netlify.app",
+  },
+  {
+    id: 8,
+    name: "Ip address tracker",
+    descShort:
+      "Ip Address Tracker let's users see their own Ip address information aswell as search for details about other Ip addresses.",
+    descLong:
+      "Ip address tracker allows users to search for details about a specific Ip address, while also showing details about the users Ip address. It also has an interactive map which shows the location of the users initial ip address location and any seearched for ip addresses.",
+    thumbNail: "where-in-the-world-thumb.png",
+    deviceMockupsSml: "device-mockups-small.png",
+    deviceMockupsLge: "device-mockups-large.png",
+    technologiesUsed: [
+      "HTML",
+      "CSS/SCSS",
+      "Javascript",
+      "React",
+      "Redux-Toolkit",
+      "Axios",
+      "Git",
+      "Leaflet",
+    ],
+    repoUrl: "https://github.com/David-Henery4/IP-Address-Tracker",
+    liveUrl: "https://ip-address-tracker-mentor.netlify.app",
+  },
+  {
+    id: 9,
+    name: "Languisa",
+    descShort:
+      "Languisa is a fictional language learning site which offers users different ways of learning languages. It also has a greeting animation in different languages.",
+    descLong:
+      "Languisa is a fictional language learning site. Which offers users multiple ways of learning a new language. The site has a small greetings animation, which displays greetings in different languages. It also has a slideshow section showcasing various testimonials",
+    thumbNail: "where-in-the-world-thumb.png",
+    deviceMockupsSml: "device-mockups-small.png",
+    deviceMockupsLge: "device-mockups-large.png",
+    technologiesUsed: ["HTML", "CSS/SCSS", "Javascript", "Git", "Parcel"],
+    repoUrl: "https://github.com/David-Henery4/Languisa",
+    liveUrl: "https://languisa.netlify.app",
+  },
+  {
+    id: 10,
+    name: "Mars Weather",
+    descShort:
+      "Gives the most up to date and available weather data on mars, from nasa's curiosity rover.",
+    descLong:
+      "Provides the users with weather data from mars, from nasa's curiosity rover. It allows them to see the weather for the most current day aswell as the previous week. Also contains a fahrenheit to celsius converter.",
+    thumbNail: "where-in-the-world-thumb.png",
+    deviceMockupsSml: "device-mockups-small.png",
+    deviceMockupsLge: "device-mockups-large.png",
+    technologiesUsed: ["HTML", "CSS", "Javascript", "Git", "Parcel"],
+    repoUrl: "https://github.com/David-Henery4/Mars-Weather",
+    liveUrl: "https://mars-tiempo.netlify.app",
+  },
+  {
+    id: 11,
+    name: "myTeam",
+    descShort:
+      "This is a dashboard style site for people to check for basic information about a football team.",
+    descLong:
+      "myTeam lets users search for stats & information about their favourite football teams. Users can get different statistics about their teams current season and can get information about their teams history and more! myTeam also has a light mode & darkmode for the users preference.",
+    thumbNail: "where-in-the-world-thumb.png",
+    deviceMockupsSml: "device-mockups-small.png",
+    deviceMockupsLge: "device-mockups-large.png",
+    technologiesUsed: [
+      "HTML",
+      "CSS/SCSS",
+      "Javascript",
+      "Chart.js",
+      "Parcel",
+      "Git",
+    ],
+    repoUrl: "https://github.com/David-Henery4/myTeam-Football-Dashboard",
+    liveUrl: "https://myteam-football-dashboard.netlify.app",
+  },
+  {
+    id: 12,
+    name: "Pomodoro",
+    descShort:
+      "A pomodoro timer for users to keep track of time using the pomodoro technique.",
+    descLong:
+      "This app lets users to use a timer when using the pomodoro technique. It also has different options to allow the user to select a timer for their break time, one for a short break and one for a long break. The app also has a settings menu so users can select custom durations for their timers, as well as being able to choose different font types & a different colour scheme.",
+    thumbNail: "where-in-the-world-thumb.png",
+    deviceMockupsSml: "device-mockups-small.png",
+    deviceMockupsLge: "device-mockups-large.png",
+    technologiesUsed: [
+      "HTML",
+      "CSS/SCSS",
+      "Javascript",
+      "React",
+      "TailwindCSS",
+    ],
+    repoUrl: "https://github.com/David-Henery4/Pomodoro-App",
+    liveUrl: "https://pomodoro-countdown-app.netlify.app",
   },
 ];
 
