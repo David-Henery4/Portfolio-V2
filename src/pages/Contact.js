70b7926b727ede7a7adf631f25b33a7cb89a39fc
DizzlyD <david4henery00@hotmail.com>
2024-06-22 23:03:48 +0100

    added-env-variable

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { BgCircle } from "../assets";
 import { ContactForm } from "../components";
 
+
 const Contact = () => {
   return (
     <motion.main
